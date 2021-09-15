dc22c0b03957b215c062cb515a01cc9a9cfddcc0
Shrestho <sadrilaminshrestho@gmail.com>
2021-09-16 00:07:10 +0600

    details added

---
+++ images/meta_data.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="refresh" content="30">
+<meta name="description" content="NexaSoft Limited || Creative art of scripts. Want to get in touch with us? Email us: coffee@nexasoft.co or Call us: +8801867009008">
+<meta name="author" content="Shrestho">
+<meta name="keywords" content="NexaSoft, nexasoft, nexasoft inc, nexasoft incorporated, nexasoft, web development, web design, mobile application development, web sever, cloud solution, ronginc, rongin, rong incorporated, ronginweb, rongincloud, web hosting, server management, cloud servers solutions">
+
+<meta property="og:title" content="NexaSoft Limited || Creative Art of Scripts">
+<meta property="og:description" content="NexaSoft Limited || Creative art of scripts. Want to get in touch with us? Email us: coffee@nexasoft.co or Call us: +8801867009008">
+<meta property="og:image" content="screenshot.png">
+<meta property="og:url" content="https://nexasoft.co/">
+<meta name="twitter:card" content="summary_large_image">
+
+<meta property="og:site_name" content="NexaSoft Limited || Creative Art of Scripts">
+<meta name="twitter:image:alt" content="NexaSoft Limited || Creative Art of Scripts">
+
+<!-- <meta property="fb:app_id" content="your_app_id" />
+<meta name="twitter:site" content="@website-username"> -->
+
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Shrestho DCosta || Future Full Stack Web Developer</title>
+      <!-- <?php require_once 'meta_data.php'; ?> -->
+      <link rel="stylesheet" href="css/skeleton.css">
+      <link rel="icon" href="images/favicon.png">
+   </head>
+   <body>
+      <div class="container">
+         <div class="row">
+            <div class="one-half column" style="margin-top: 25%">
+               <h4 style="font-size:25pt;font-weight:bold;color:black">
+                  <span style="color:springgreen;font-size:40pt;">                  
+                  </span>
+                  ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّٰهِ وَبَرَكَاتُهُ‎‎
+                  <br> 
+                  <span style="font-size:30pt;">Assalamu Alaikum</span><span style="color:springgreen;font-size:40pt; ">.</span>
+                  <br>
+                  <span style="font-size:26pt;">We'll be back soon.
+                  </span>
+                  <br>
+                  <span style="color:springgreen;font-size:25pt;">hello</span><span style="font-size:25pt;">@shrestho.me</span>
+                  <br>
+               </h4>
+            </div>
+         </div>
+      </div>
+<!-- <?php require_once 'analytics.php'; ?> -->
+   </body>
+</html>
